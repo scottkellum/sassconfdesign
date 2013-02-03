e7e0851d5f5161da4849282e991255827554b281
Scott Kellum <scott@scottkellum.com>
2013-02-03 18:44:47 -0500

    Getting this style guide going

---
+++ website/config.rb
@@ -0,0 +1,23 @@
+# Require any additional compass plugins here.
+
+require 'compass'
+require 'toolkit'
+require 'color-schemer'
+
+# Set this to the root of your project when deployed:
+sass_dir         = "sass"
+http_path        = "public"
+css_dir          = "public/lib/css"
+javascripts_dir  = "public/lib/js"
+images_dir       = "public/lib/img"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style     = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments    = false
+
+preferred_syntax = :sass
