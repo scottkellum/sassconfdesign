59a65f8e828a112df9d011dbfb49c1d782819005
Eric J. Duran <eric.duran7@gmail.com>
2013-02-11 18:39:18 -0500

    Adding basic JS to prevent empty from from being submitted.
    Note: I didn't actually include the JS since I didn't have an error to show.
    Also I kept it vanilla JS for now I figured the whole site will be pretty light

---
+++ website/public/lib/js/main.js
@@ -0,0 +1,22 @@
+/**
+ * Keeping it simple for now.
+ * We can add jQuery if everyone is ok with that.
+ */
+(function (doc) {
+  if (doc.addEventListener && doc.querySelector) {
+    doc.addEventListener('DOMContentLoaded', function() {
+      // Add our event listener to the form.
+      var mailForm = doc.querySelector('#mc-embedded-subscribe-form'),
+          emailField = doc.querySelector('#mce-EMAIL');
+
+      mailForm.addEventListener('submit', function(e) {
+        // Make sure the email field is field out.
+        if (emailField.value === '') {
+          e.preventDefault();
+          // Show Email Waiting to display email.
+          // Alternative we could just add the require attribute to the <input>
+        }
+      }, false)
+    });
+  }
+})(document);
