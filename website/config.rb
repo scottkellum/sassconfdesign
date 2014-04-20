2257c85a74a662daa34e321c1c4463060fa4da0b
Scott Kellum <scott@scottkellum.com>
2014-04-20 15:37:01 -0400

    New middleman instance

--- website/config.rb
+++ website/config.rb
@@ -1,53 +1,15 @@
 ###
-# Blog settings
-###
-
-# Time.zone = "UTC"
-
-activate :blog do |blog|
-  blog.prefix = "pages"
-  blog.permalink = ":title.html"
-  # blog.sources = ":year-:month-:day-:title.html"
-  # blog.taglink = "tags/:tag.html"
-  # blog.layout = "blog"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.year_link = ":year.html"
-  # blog.month_link = ":year/:month.html"
-  # blog.day_link = ":year/:month/:day.html"
-  # blog.default_extension = ".markdown"
-
-  blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
-
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/:num"
-end
-
-page "/feed.xml", :layout => false
-
-###
 # Compass
 ###
 
-# Susy grids in Compass
-# First: gem install susy
-require 'toolkit'
-require 'color-schemer'
-require 'modular-scale'
-require 'singularitygs'
-
 # Change Compass configuration
 # compass_config do |config|
-  config.output_style = :compressed
+#   config.output_style = :compact
 # end
 
 ###
 # Page options, layouts, aliases and proxies
 ###
-
-activate :directory_indexes
 
 # Per-page layout changes:
 #
@@ -55,20 +17,16 @@ activate :directory_indexes
 # page "/path/to/file.html", :layout => false
 #
 # With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
 #
 # A path which all have the same layout
 # with_layout :admin do
-  page "/*", :layout => "layout/_layout"
-  page "/pages/*", :layout => "layout/_post"
-  page "/calendar.html", :layout => "layout/_blog"
-  page "/tag.html", :layout => "layout/_blog"
-  page "/pages/index.html", :layout => "layout/_blog"
+#   page "/admin/*"
 # end
 
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
 
 ###
 # Helpers
@@ -76,6 +34,9 @@ activate :directory_indexes
 
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -93,25 +54,17 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  activate :minify_css
+  # activate :minify_css
 
   # Minify Javascript on build
-  activate :minify_javascript
-
-  # Gzip html/css/js
-  activate :gzip
+  # activate :minify_javascript
 
   # Enable cache buster
-  activate :cache_buster
+  # activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
 
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-
   # Or use a different image path
-  # set :http_path, "/Content/images/"
+  # set :http_prefix, "/Content/images/"
 end
